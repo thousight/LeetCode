e52c6a9e7bdce0aea9bb6579eaab1ee8aa47d68d
Mark Wen <markwenguojie94@gmail.com>
2017-10-31 19:49:54 -0400

    Set up environment

--- src/space/markwen/Main.java
+++ src/space/markwen/Main.java
@@ -4,5 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println(ThreeSums.threeSumClosest(new int[]{-1, 2, 1, -4}, 1));
     }
 }
